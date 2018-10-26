2e971edc824ce390be985f3335b080bbf961d5d9
ByronCrowhurst <bc216857@falmouth.ac.uk>
2018-10-26 03:46:13 +0100

    Algorithms now work. No comments yet, more optimisation could be made.

--- README.md
+++ README.md
@@ -6,5 +6,21 @@
 The man function calls every other function neatly in one place, it calls the load_image function and stores the returned value in variables to be passed to the anomalous_trichromacy 
 2. load_image\(\)
 The load image function simply loads and returns screenshot of the game to be changed later on in the code.
-3. anomalous_trichromacy\(\)
-This algorthm takes the images and converts them to a pixel array, then reduces the RGB values of the pixel arrays. Then, once written to the image, deletes the arrays and saves the images.
+3. colour_blind_adjustments\(\)
+This algorthm takes the images and converts them to a pixel array, then passes the values of the pixel arrays to a seperate reduction algorithm. Then, once assigned to the array it is then deleted so that pygame can save the images.
+4. protanopia_recolouring\(\)
+This algorithm takes the RGB values, then using the correct numbers\* adjusts the values accordingly and returns the new RGB values
+
+\*RGB channel values:
+
+Normal: R:\[100, 0, 0\], G:\[0, 100, 0\], B:\[0, 100, 0]
+ Protanopia: R:\[56\.667, 43\.333, 0\], G:\[55\.833, 44\.167, 0\], B:\[0, 24\.167, 75\.833\]
+ Protanomaly: R:\[81\.667, 18\.333, 0\], G:\[33\.333, 66\.667, 0\], B:\[0, 12\.5, 87\.5]
+ Deuteranopia: R:\[62\.5, 37\.5, 0\], G:\[70, 30, 0\], B:\[0, 30, 70\]
+ Deuteranomaly: R:\[80, 20, 0\], G:\[25\.833, 74\.167, 0\], B:\[0, 14\.167, 85\.833\]
+ Tritanopia: R:\[95, 5, 0\], G:\[0, 43\.333, 56\.667\], B:\[0, 47\.5, 52\.5\]
+ Tritanomaly: R:\[96\.667, 3\.333, 0\], G:\[0, 73\.333, 26\.667\], B:\[0, 18\.333, 81\.667\]
+ Achromatopsia: R:\[29\.9, 58\.7, 11\.4\], G:\[29\.9, 58\.7, 11\.4\], B:\[29\.9, 58\.7, 11\.4\]
+ Achromatomaly: R:\[61\.8, 32, 6\.2\], G:\[16\.3, 77\.5, 6\.2\], B:\[16\.3, 32\.0, 51\.6\]
+
+extracted from: http://web.archive.org/web/20081014161121/http://www.colorjack.com/labs/colormatrix/
--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def main():
     image_one = load_image()
     image_two = load_image()
     image_three = load_image()
-    anomalous_trichromacy(base_image, image_one, image_two, image_three)
+    colour_blind_adjustments(base_image, image_one, image_two, image_three)
 
 
 def load_image():
@@ -20,11 +20,11 @@ def load_image():
     return image
 
 
-def anomalous_trichromacy(base_image, image_one, image_two, image_three):
-    protanomaly = image_one
+def colour_blind_adjustments(base_image, image_one, image_two, image_three):
+    protanopia_image = image_one
     deuteranomaly = image_two
     tritanomaly = image_three
-    protanomaly_colour = pygame.PixelArray(protanomaly)
+    protanomaly_colour = pygame.PixelArray(protanopia_image)
     deuteranomaly_colour = pygame.PixelArray(deuteranomaly)
     tritanomaly_colour = pygame.PixelArray(tritanomaly)
     image_width, image_height = base_image.get_size()
@@ -33,19 +33,67 @@ def anomalous_trichromacy(base_image, image_one, image_two, image_three):
             red = base_image.get_at((x, y)).r
             green = base_image.get_at((x, y)).g
             blue = base_image.get_at((x, y)).b
-            proto_red = (red*0.15)
-            deuter_green = (green*0.15)
-            trit_blue = (blue*0.15)
-            protanomaly_colour[x, y] = (proto_red, green, blue)
-            deuteranomaly_colour[x, y] = (red, deuter_green, blue)
-            tritanomaly_colour[x, y] = (red, green, trit_blue)
+            proto_red, proto_green, proto_blue = protanopia_recolouring(red, green, blue)
+            deuta_red, deuta_green, deuta_blue = deuternopia_recolouring(red, green, blue)
+            trita_red, trita_green, trita_blue = tritanopia_recolouring(red, green, blue)
+            protanomaly_colour[x, y] = (proto_blue, proto_green, proto_red)
+            deuteranomaly_colour[x, y] = (deuta_blue, deuta_green, deuta_red)
+            tritanomaly_colour[x, y] = (trita_blue, trita_green, trita_red)
 
     del protanomaly_colour
     del deuteranomaly_colour
     del tritanomaly_colour
-    pygame.image.save(protanomaly, "protoanomaly screen.PNG")
+    pygame.image.save(protanopia_image, "protoanomaly screen.PNG")
     pygame.image.save(deuteranomaly, "deuteranomaly screen.PNG")
     pygame.image.save(tritanomaly, "tritanomaly.screen.PNG")
+
+
+def protanopia_recolouring(red, green, blue):
+    red_channel_red_value = red * 0.56667
+    red_channel_green_value = green * 0.43337
+    red_channel_blue_value = blue * 0.0
+    green_channel_red_value = red * 0.55883
+    green_channel_green_value = green * 0.44167
+    green_channel_blue_value = blue * 0.0
+    blue_channel_red_value = red * 0.0
+    blue_channel_green_value = green * 0.24167
+    blue_channel_blue_value = blue * 0.75833
+    red = red_channel_red_value + red_channel_green_value + red_channel_blue_value
+    green = green_channel_red_value + green_channel_green_value + green_channel_blue_value
+    blue = blue_channel_red_value + blue_channel_green_value + blue_channel_blue_value
+    return red, green, blue
+
+
+def deuternopia_recolouring(red, green, blue):
+    red_channel_red_value = red * 0.625
+    red_channel_green_value = green * 0.375
+    red_channel_blue_value = blue * 0.0
+    green_channel_red_value = red * 0.7
+    green_channel_green_value = green * 0.3
+    green_channel_blue_value = blue * 0.0
+    blue_channel_red_value = red * 0.0
+    blue_channel_green_value = green * 0.3
+    blue_channel_blue_value = blue * 0.7
+    red = red_channel_red_value + red_channel_green_value + red_channel_blue_value
+    green = green_channel_red_value + green_channel_green_value + green_channel_blue_value
+    blue = blue_channel_red_value + blue_channel_green_value + blue_channel_blue_value
+    return red, green, blue
+
+
+def tritanopia_recolouring(red, green, blue):
+    red_channel_red_value = red * 0.95
+    red_channel_green_value = green * 0.05
+    red_channel_blue_value = blue * 0.0
+    green_channel_red_value = red * 0.0
+    green_channel_green_value = green * 0.43333
+    green_channel_blue_value = blue * 0.56667
+    blue_channel_red_value = red * 0.0
+    blue_channel_green_value = green * 0.475
+    blue_channel_blue_value = blue * 0.525
+    red = red_channel_red_value + red_channel_green_value + red_channel_blue_value
+    green = green_channel_red_value + green_channel_green_value + green_channel_blue_value
+    blue = blue_channel_red_value + blue_channel_green_value + blue_channel_blue_value
+    return red, green, blue
 
 
 main()
